2b0a3daf9ddf3a6d71f537f20981a221956d0f84
gabrielnp <gabrielnp.2010@gmail.com>
2021-08-08 18:06:40 -0300

    feat: add update user endpoint
    CHANGE LOG:
    add endpoint to update users data like name, phone number and address

--- app/users/users.py
+++ app/users/users.py
@@ -1,7 +1,10 @@
-from flask import jsonify, request, make_response
+import json
+
+from flask import request, make_response
 from flask.blueprints import Blueprint
 
 from app.models import User
+from app.utils.db import db
 
 
 user_bp = Blueprint('user_bp', __name__, url_prefix='/users')
@@ -24,5 +27,28 @@ def retrieve_by_id(user_id):
         if user:
             return user.as_dict()
         return make_response({}), 200
+    except Exception as e:
+        return make_response({'error': e}), 400
+
+@user_bp.route('/<user_id>', methods=['PATCH'])
+def update(user_id):
+    try:
+        data =json.loads(request.data)
+        user = User.query.filter_by(id=user_id).first()
+
+        if 'first_name' in data:
+            user.first_name = data['first_name']
+
+        if 'last_name' in data:
+            user.last_name = data['last_name']
+        
+        if 'phone' in data:
+            user.phone = data['phone']
+        
+        if 'address' in data:
+            user.address = data['address']
+
+        db.session.commit()
+        return user.as_dict()
     except Exception as e:
         return make_response({'error': e}), 400
