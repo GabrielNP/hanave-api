9b36bc7f3128f86c8a22a6ff6cb1bd63d8d77e98
gabrielnp <gabrielnp.2010@gmail.com>
2021-08-11 21:56:59 -0300

    fix: redo default values

--- migrations/20210810213500_alter.sql
+++ migrations/20210810213500_alter.sql
@@ -3,7 +3,7 @@ ALTER TABLE public.purchases ADD color varchar NOT NULL;
 ALTER TABLE public.products ADD size_available _text NOT NULL DEFAULT '{S, M, B}';
 ALTER TABLE public.products ADD color_available _text NOT NULL DEFAULT '{"black", "red"}';
 
-ALTER TABLE public.purchases ADD status varchar NOT NULL DEFAULT 'pagamento pendente';
+ALTER TABLE public.purchases ADD status varchar NOT NULL DEFAULT 'pending_payment';
 ALTER TABLE public.purchases ADD created_at timestamp NOT NULL DEFAULT now();
 ALTER TABLE public.purchases ADD updated_at timestamptz NOT NULL DEFAULT now();
 ALTER TABLE public.purchases ADD canceled_at timestamptz NULL;
