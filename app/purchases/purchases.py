a412b5ccfb813757649cbfa7281df5bdd00b6a2e
gabrielnp <gabrielnp.2010@gmail.com>
2021-08-14 10:04:46 -0300

    feat: add assets

--- app/purchases/purchases.py
+++ app/purchases/purchases.py
@@ -7,13 +7,15 @@ from uuid import uuid4
 
 from flask import make_response, request, jsonify
 from flask.blueprints import Blueprint
-import pytz
-from sqlalchemy import desc
+from sqlalchemy import create_engine, desc
+from sqlalchemy.sql.expression import text
 from unidecode import unidecode
 
-from app.models import Purchase
-from app.utils.db import db
+from app.models import Product, Purchase
+from app.utils.db import Parser, db
 from app.utils import purchase_status as STATUS
+
+SQL_ENGINE = create_engine(os.getenv('DATABASE_URL'), client_encoding='utf8', implicit_returning=True)
 
 
 purchase_bp = Blueprint('purchase_bp', __name__, url_prefix='/purchases')
@@ -22,7 +24,91 @@ purchase_bp = Blueprint('purchase_bp', __name__, url_prefix='/purchases')
 def get_by_user(user_id):
     try:
         purchases = Purchase.query.filter_by(user_id=user_id).order_by(desc(Purchase.created_at)).all()
-        return jsonify([i.serialize for i in purchases])
+        # TODO: return also relational product names 
+        # return jsonify([i.serialize for i in purchases])
+
+        obj = [
+            {
+                "address": {
+                "city": "San Antonio",
+                "geolocation": {
+                    "lat": "50.3467",
+                    "long": "-20.1310"
+                },
+                "number": 6454,
+                "street": "Hunters Creek Dr",
+                "zipcode": "98234-1734"
+                },
+                "canceled_at": None,
+                "color": "red",
+                "created_at": "2021-08-13T22:50:22+01:00",
+                "delivery_forecast": "2021-09-13T23:05:19.173205+01:00",
+                "payment_type": "card",
+                "product_id": 4,
+                "purchase_code": 1628895022,
+                "purchase_id": "7d9f78cb-4daa-40a2-b97b-f316d95968dd",
+                "reason": None,
+                "size": "B",
+                "status": "paid",
+                "updated_at": "2021-08-13T23:05:19.173205+01:00",
+                "user_id": 7,
+                "category": "Camiseta Masculina Manga Longa",
+                "color_available": [
+                    "black"
+                ],
+                "description": "Camiseta Masculina De Manga Longa",
+                "id": 4,
+                "image": "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg",
+                "price": 34.9,
+                "size_available": [
+                    "S",
+                    "M",
+                    "B"
+                ],
+                "title": "Camiseta Manga Longa"
+            },
+            {
+                "address": {
+                "city": "San Antonio",
+                "geolocation": {
+                    "lat": "50.3467",
+                    "long": "-20.1310"
+                },
+                "number": 6454,
+                "street": "Hunters Creek Dr",
+                "zipcode": "98234-1734"
+                },
+                "canceled_at": None,
+                "color": "red",
+                "created_at": "2021-06-13T22:55:28+01:00",
+                "delivery_forecast": None,
+                "payment_type": "card",
+                "product_id": 4,
+                "purchase_code": 93741628895328,
+                "purchase_id": "d679fa34-6b77-4fb0-9c87-576a6a7ae870",
+                "reason": None,
+                "size": "B",
+                "status": "pending_payment",
+                "updated_at": "2021-08-13T22:55:28.089544+01:00",
+                "user_id": 7,
+                "category": "Sobretudo Feminino",
+                "color_available": [
+                    "lilac"
+                ],
+                "description": "Sobretudo Para o Inverno Lilas",
+                "id": 9,
+                "image": "https://fakestoreapi.com/img/51Y5NI-I5jL._AC_UX679_.jpg",
+                "price": 120.0,
+                "size_available": [
+                    "S",
+                    "M",
+                    "B"
+                ],
+                "title": "Sobretudo Inverno"
+            }
+        ]
+       
+        return jsonify(obj)
     except Exception as e:
         return make_response({'error': str(e)}), 400
 
